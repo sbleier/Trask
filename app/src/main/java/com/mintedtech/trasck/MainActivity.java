6c41d56f5a59a0e3e3f4517749b42dfb7c52d248
sbleier <sbleier2@student.touro.edu>
2024-08-14 11:58:47 -0400

    git commit -m "commit" git push origin main

--- app/src/main/java/com/mintedtech/trasck/MainActivity.java
+++ app/src/main/java/com/mintedtech/trasck/MainActivity.java
@@ -9,6 +9,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.mintedtech.trasck.databinding.ActivityMainBinding;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -60,6 +62,28 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this, TaskActivity.class);
         startActivity(intent);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/com/mintedtech/trasck/TaskActivity.java
+++ app/src/main/java/com/mintedtech/trasck/TaskActivity.java
@@ -2,6 +2,7 @@ package com.mintedtech.trasck;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.NonNull;
@@ -38,6 +39,7 @@ public class TaskActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
+        setupBackArrow();
 
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +53,11 @@ public class TaskActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(view -> pauseResumeTimer());
         setupTimer();
         pauseAndResetTimerOnInitialRun(savedInstanceState);
+    }
+
+    private void setupBackArrow() {
+        FloatingActionButton backArrow = findViewById(R.id.backArrow);
+        backArrow.setOnClickListener(view -> onBackPressed());
     }
 
     private void setupTimer() {
@@ -140,28 +147,15 @@ public class TaskActivity extends AppCompatActivity {
         updateTextView();
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
             return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        } else
+            return super.onOptionsItemSelected(item);
     }
+
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
